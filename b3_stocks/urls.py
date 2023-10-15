89d732f9f1c258be05652a352118ce68ee76d90a
vini-machado <viniciuscmachado22@gmail.com>
2023-10-15 08:45:05 -0300

    start authentication app

--- b3_stocks/urls.py
+++ b3_stocks/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stocks/', include('stock_fetcher.urls')),
+    path('authentication/', include('authentication.urls')),
 ]
