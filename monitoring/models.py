c806341cd412496b07bb958247eec202849115e6
vini-machado <viniciuscmachado22@gmail.com>
2023-10-15 17:52:50 -0300

    create "create_or_update" method on UserStock model

--- monitoring/models.py
+++ monitoring/models.py
@@ -9,3 +9,19 @@ class UserStock(models.Model):
     max_price = models.DecimalField(max_digits=6, decimal_places=2, default = 0)
     min_price = models.DecimalField(max_digits=6, decimal_places=2, default = 0)
     periodicity = models.CharField(max_length=3, null=True)
+
+    @classmethod
+    def create_or_update(self, user, stock, max_price = 0, min_price = 0, periodicity = None):
+        defaults = {
+            'max_price' : max_price,
+            'min_price' : min_price,
+            'periodicity' : periodicity
+        }
+
+        obj, created = self.objects.get_or_create(user=user, stock=stock, defaults=defaults)
+        if not created:
+            obj.max_price = defaults.get('max_price')
+            obj.min_price = defaults.get('min_price')
+            obj.periodicity = defaults.get('periodicity')
+            
+            obj.save()
