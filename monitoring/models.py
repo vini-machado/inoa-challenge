6f5f65dd3c0d79c41dadc68e0a628e48d14b26ea
vini-machado <viniciuscmachado22@gmail.com>
2023-10-15 17:01:36 -0300

    modifying user_stock price constraints

--- monitoring/models.py
+++ monitoring/models.py
@@ -6,6 +6,6 @@ class UserStock(models.Model):
     user  = models.ForeignKey(User, on_delete=models.CASCADE)
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     
-    max_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
-    min_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    max_price = models.DecimalField(max_digits=6, decimal_places=2, default = 0)
+    min_price = models.DecimalField(max_digits=6, decimal_places=2, default = 0)
     periodicity = models.CharField(max_length=3, null=True)
