139822d8e1d4a5ac691f21debe3b9d782fdb5659
vini-machado <viniciuscmachado22@gmail.com>
2023-10-15 21:14:43 -0300

    tunnel price inputs working

--- stock_fetcher/views/stock_data.py
+++ stock_fetcher/views/stock_data.py
@@ -2,12 +2,16 @@ from typing import Any
 from django.shortcuts import render
 from django.views import View
 from django import forms
-from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
+from django.contrib import messages
+from django.shortcuts import redirect
+
 import pandas as pd
+
 from ..utils.stock_handler import StockHandler
 from ..utils.stock_chart import StockChart
 from stock_fetcher import INTERVALS, PERIODS
+from monitoring.models import UserStock
+from ..models import Stock
 
 class TimeForm(forms.Form):
     interval = forms.ChoiceField(choices = INTERVALS,
@@ -17,35 +21,45 @@ class TimeForm(forms.Form):
     period   = forms.ChoiceField(choices = PERIODS,
                                     widget=forms.Select(attrs={'class': 'form-select'}),
                                     label="Select Time Period")
+    
+class TunnelPriceForm(forms.Form):
+    max_price = forms.DecimalField(label="Select Tunnel Maximum Price")
+    min_price = forms.DecimalField(label="Select Tunnel Minimum Price")
+
 
 class StockDataView(View):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
 
         self.template_name = 'stock_data.html'
-        self.sh = StockHandler()
-        self.context = dict()
+        self.sh            = StockHandler()
+        self.context       = dict()
+        self.ticker        = None
+        self.interval      = '1m'
+        self.period        = '5d'
+        self.user_stock    = None
 
-    @method_decorator(cache_page(60 * 5))
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, ticker: str):
-        interval = request.GET.get('interval')
-        period   = request.GET.get('period')
+        self.ticker   = ticker
+        self.interval = request.GET.get('interval')
+        self.period   = request.GET.get('period')
         
-        self.__define_context(request, ticker, interval, period)
+        self.__define_context(request)
 
-        return render(request, self.template_name , self.context)
+        return render(request, self.template_name, self.context)
         
     ############################### GET CONTEXT ##################################
-    def __define_context(self, request, ticker: str, interval: str, period: str):
-        raw_data, table_data = self.__handle_stock_data(ticker, interval, period)
+    def __define_context(self, request):
+        raw_data, table_data = self.__handle_stock_data()
+        tunnel_prices        = self.__price_tunnel_context(request)
 
-        self.context['plot']       = self.__plot_context(raw_data)
-        self.context['ticker']     = ticker
-        self.context['stock_data'] = table_data
-        self.context['form'] = TimeForm(request.GET)
+        self.context['plot']             = self.__plot_context(raw_data)
+        self.context['ticker']           = self.ticker
+        self.context['stock_data']       = table_data
+
+        self.context['price_tunnel']     = TunnelPriceForm(initial = tunnel_prices)
+        self.context['graph_properties'] = TimeForm(request.GET)
     
     def __plot_context(self, data: pd.DataFrame):
         plot_data  = data.reset_index()
@@ -54,21 +68,25 @@ class StockDataView(View):
         plot = sc.plot()
 
         return plot
-
-    # def __form_context(self, request):
-    #     form = TimeForm(request.GET)
-    #     if form.is_valid():
-    #         interval = form.cleaned_data['interval']
-    #         period   = form.cleaned_data['period']
-    #     else:
-    #         interval = "1m"
-    #         period   = "5d"
-    ############################### GET CONTEXT ##################################
-         
     
+    def __price_tunnel_context(self, request):
+        user_stock = UserStock.objects.filter(user=request.user, stock__ticker = self.ticker)
+        
+        if user_stock.exists():
+            self.user_stock = user_stock.first()
+            max_price       = float(self.user_stock.max_price)
+            min_price       = float(self.user_stock.min_price)
+
+            return { 'max_price': max_price, 'min_price': min_price }
+
+        return  { 'max_price': 0, 'min_price': 0 }
+
+    ############################### GET CONTEXT ##################################
+
+
     ############################### GET HELPERS ##################################
-    def __handle_stock_data(self, ticker: str, interval: str, period: str) -> tuple[pd.DataFrame, list[dict]]:
-        raw_data = self.sh.get_stock_data(ticker, interval, period)
+    def __handle_stock_data(self) -> tuple[pd.DataFrame, list[dict]]:
+        raw_data = self.sh.get_stock_data(self.ticker, self.interval, self.period)
 
         stock_data = self.__modify_raw_data(raw_data.copy())
         stock_data = stock_data.to_dict('records')
@@ -89,3 +107,21 @@ class StockDataView(View):
 
         return raw_data
     ############################### GET HELPERS ##################################
+
+    ############################### POST ##################################
+    def post(self, request, ticker):
+        tunnel_price = TunnelPriceForm(request.POST)
+        
+        self.user_stock = UserStock.objects.filter(user = request.user, stock__ticker = ticker).first()
+
+        if tunnel_price.is_valid():
+
+            self.user_stock.max_price   = request.POST.get('max_price')
+            self.user_stock.min_price   = request.POST.get('min_price')
+            self.user_stock.periodicity = self.period
+
+            self.user_stock.save()
+            messages.success(request = request, message = "Success Registration")
+
+        return redirect(request.get_full_path())
+    ############################### POST  ##################################
