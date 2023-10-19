6ad0bca65f1344b84afda51fcf2facabe738b743
vini-machado <viniciuscmachado22@gmail.com>
2023-10-18 23:02:00 -0300

    include all 'templates' folders

--- b3_stocks/settings.py
+++ b3_stocks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import json
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'b3_stocks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'b3_stocks', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
