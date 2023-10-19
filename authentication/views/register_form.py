d985db37f441694e5d7769806d296ec7886c1c64
vini-machado <viniciuscmachado22@gmail.com>
2023-10-19 12:15:35 -0300

    authentication css working

--- authentication/views/register_form.py
+++ authentication/views/register_form.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
     class Meta:
-        model=User
-        fields = ['username','email','password1','password2']
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
